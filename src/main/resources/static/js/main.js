a49c41303f9100446261234feafb8b93dbd42402
Draogol <dragonultimateproject@gmail.com>
2020-07-21 18:01:07 +0300

    скрипт

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,5 @@
+//var ClickOnBut = document.getElementById('submitID');
+function StartMes(){
+	'use strict';
+	alert('Номер паспорта: ' + document.getElementById('PassportNumber').value + ' Имя: ' + document.getElementById('Name').value);
+}
