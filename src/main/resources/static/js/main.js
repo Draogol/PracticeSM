5fc27b2b07847a52f8a1c78794ec299b34092cc2
Draogol <dragonultimateproject@gmail.com>
2020-07-21 23:16:54 +0300

    UpdateScr

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,5 +1,16 @@
 //var ClickOnBut = document.getElementById('submitID');
+function testF(Num, Name, Pass){
+	'use strict';
+	$('.newIndex').html(Num);
+	$('.newName').html(Name);
+	$('.newNum').html(Pass);
+}
 function StartMes(){
 	'use strict';
-	alert('Номер паспорта: ' + document.getElementById('PassportNumber').value + ' Имя: ' + document.getElementById('Name').value);
+	//alert('Номер паспорта: ' + document.getElementById('PassportNumber').value + ' Имя: ' + document.getElementById('Name').value);
+	if (document.getElementById('Name').value !== ""){
+		if (document.getElementById('PassportNumber').value !== ""){
+			alert(document.location.host + '/add?name=' + document.getElementById('Name').value + '&passportNumber=' + document.getElementById('PassportNumber').value);
+		}
+	}
 }
